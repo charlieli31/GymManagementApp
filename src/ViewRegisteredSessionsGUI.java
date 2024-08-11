3fda165ba176b411283793901ab0a13e89ab507f
alex <asm8755@nyu.edu>
2024-08-11 19:29:18 -0400

    -

--- src/ViewRegisteredSessionsGUI.java
+++ src/ViewRegisteredSessionsGUI.java
@@ -1,12 +1,18 @@
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class ViewRegisteredSessionsGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JButton btnReschedule;
+	private JButton btnCancelSession;
+	private JButton btnCancel;
+	private JList sessions;
 
 
 
@@ -20,6 +26,24 @@ public class ViewRegisteredSessionsGUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		btnReschedule = new JButton("Reschedule");
+		btnReschedule.setBounds(37, 186, 115, 29);
+		//btnReschedule.setBounds(100, 100, 450, 300);
+		contentPane.add(btnReschedule);
+		
+		
+		btnCancel = new JButton("Cancel");
+		btnCancel.setBounds(334, 186, 86, 29);
+		//btnCancel.setBounds(100, 100, 450, 300);
+		contentPane.add(btnCancel);
+		
+		btnCancelSession = new JButton("Cancel Session");
+		btnCancelSession.setBounds(178, 193, 138, 15);
+		contentPane.add(btnCancelSession);
+		
+		
 	}
 
 }
