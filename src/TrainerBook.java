154741b7c23dd7c4b9c2464610cc6ba4f4526dd6
Eli Sun <54780075+IDislikeName@users.noreply.github.com>
2024-08-11 15:43:52 -0400

    Update TrainerBook.java

--- src/TrainerBook.java
+++ src/TrainerBook.java
@@ -1,4 +1,72 @@
+import java.io.Serializable;
 
-public class TrainerBook {
+import java.util.ArrayList;
+
+
+public class TrainerBook implements Serializable{
+
+	private ArrayList<Trainer> trainers;
+
+
+	
+
+	public TrainerBook() {
+
+		this.trainers = new ArrayList<Trainer>();
+
+		initializeTrainers();
+
+		
+
+	}
+
+	
+
+	
+
+	void initializeTrainers() {
+
+		Trainer trainer1 = new Trainer("Alex Manko");
+
+		Trainer trainer2 = new Trainer("Eli Sun");
+
+		Trainer trainer3 = new Trainer("Charlie Li");
+
+		Trainer trainer4 = new Trainer("LeBron James");
+
+		Trainer trainer5 = new Trainer("Kevin Durant");
+
+		
+
+		 for (int i = 1; i <= 5; i++) {
+
+	            trainer1.addSession(new Session("2024-08-" + i, "10:00 AM"));
+
+	            trainer2.addSession(new Session("2024-08-" + i, "11:00 AM"));
+
+	            trainer3.addSession(new Session("2024-08-" + i, "12:00 PM"));
+
+	            trainer4.addSession(new Session("2024-08-" + i, "01:00 PM"));
+
+	            trainer5.addSession(new Session("2024-08-" + i, "02:00 PM"));
+
+	        }
+
+		 
+
+		trainers.add(trainer1);
+
+        trainers.add(trainer2);
+
+        trainers.add(trainer3);
+
+        trainers.add(trainer4);
+
+        trainers.add(trainer5);
+
+
+	}
+
+		
 
 }
