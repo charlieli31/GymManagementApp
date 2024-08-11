8dcc064859c12d375c95fd4d5980d4f4e06c54d4
alexmank <90431616+alexmank@users.noreply.github.com>
2024-08-11 00:00:41 -0400

    Update MemberBook.java

--- GymManagementApp/src/MemberBook.java
+++ GymManagementApp/src/MemberBook.java
@@ -1,4 +1,16 @@
+import java.util.*;
+
 
 public class MemberBook {
+	private ArrayList<Member> memberBook;
 
+	public ArrayList<Member> getMemberBook() {
+		return memberBook;
+	}
+
+	public void setMemberBook(ArrayList<Member> memberBook) {
+		this.memberBook = memberBook;
+	}
+	
+	
 }
